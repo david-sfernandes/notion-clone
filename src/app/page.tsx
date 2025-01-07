b168e73e35095ec2789212d678843ef19a4b68c6
David Fernandes <davidsou.fer@gmail.com>
2025-01-06 22:53:57 -0300

    Edit home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+import NewDocumentButton from "@/components/NewDocumentButton";
 
 export default function Home() {
   return (
-    <div>
-      <p>Content</p>
+    <div className="px-3 py-4 ">
+      <h3 className="text-lg font-medium">
+        Faça o ligin e crie um novo documento para começar!
+      </h3>
+      <p className="text-zinc-500 mb-2">Você também pode convidar amigos e colegas para editar os documentos em tempo real com você.</p>
+      <NewDocumentButton />
     </div>
   );
 }
